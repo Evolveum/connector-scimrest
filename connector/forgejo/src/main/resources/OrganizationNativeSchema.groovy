8f1cdf988acd88a5a564716ea4416f428ddcb5ab
Tony Tkáčik <tonydamage@gmail.com>
2025-05-09 10:18:49 +0200

    Add initial mappings and schema for Forgejo connector
    Introduce `ConnIdMapping.groovy` and `OrganizationNativeSchema.groovy` for Forgejo connector. These files define attribute mappings for `User` and `Organization` objects, and specify the schema for `Organization` attributes.

---
+++ connector/forgejo/src/main/resources/OrganizationNativeSchema.groovy
@@ -0,0 +1,30 @@
+objectClass("Organization") {
+    attribute("avatar_url") {
+        jsonType "string"
+    }
+    attribute("description") {
+        jsonType "string"
+    }
+    attribute("email") {
+        jsonType "string"
+    }
+    attribute("full_name") {
+        jsonType "string"
+    }
+    attribute("id") {
+        jsonType "integer"
+        openApiFormat "int64"
+    }
+    attribute("location") {
+        jsonType "string"
+    }
+    attribute("name") {
+        jsonType "string"
+    }
+    attribute("visibility") {
+        jsonType "string"
+    }
+    attribute("website") {
+        jsonType "string"
+    }
+}
