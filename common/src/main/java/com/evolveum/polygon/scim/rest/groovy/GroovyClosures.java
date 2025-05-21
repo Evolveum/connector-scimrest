a528ee70ea46edbac9a7b9ee19716bb182b3f5f1
Tony Tkáčik <tonydamage@gmail.com>
2025-05-21 10:04:58 +0200

    Add Javadoc to helper methods to support Groovy closures

--- common/src/main/java/com/evolveum/polygon/scim/rest/groovy/GroovyClosures.java
+++ common/src/main/java/com/evolveum/polygon/scim/rest/groovy/GroovyClosures.java
@@ -3,6 +3,15 @@ package com.evolveum.polygon.scim.rest.groovy;
 import groovy.lang.Closure;
 
 public class GroovyClosures {
+
+    /**
+     * Calls the provided closure with the specified delegate and returns the delegate.
+     *
+     * @param <T>      The type of the delegate.
+     * @param closure  The closure to be executed.
+     * @param delegate The delegate object that will be used within the closure.
+     * @return The modified delegate object after execution.
+     */
     public static <T>  T callAndReturnDelegate(Closure<?> closure, T delegate) {
         closure.setDelegate(delegate);
         closure.setResolveStrategy(Closure.DELEGATE_FIRST);
@@ -10,6 +19,14 @@ public class GroovyClosures {
         return delegate;
     }
 
+    /**
+     * Creates a copy of the provided closure and executes it with the specified delegate.
+     *
+     * @param <T> The type of the delegate.
+     * @param prototype The original closure to be copied and executed.
+     * @param delegate The delegate object that will be used within the closure.
+     * @return The result of the closure execution.
+     */
     public static <T>  T copyAndCall(Closure<?> prototype, Object delegate) {
         Closure<?> closure = (Closure<?>) prototype.clone();
         closure.setDelegate(delegate);
