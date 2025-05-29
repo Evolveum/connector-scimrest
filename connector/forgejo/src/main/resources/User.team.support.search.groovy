6e8e54dee673d63e0d6a45d5d78c47d6da4379fe
Tony Tkáčik <tonydamage@gmail.com>
2025-05-29 11:13:55 +0200

    Add team attribute resolution for Forgejo search
    This implementation enables resolving and associating users with their respective teams during searches. It ensures teams are fetched via organizational hierarchy and checks for team membership efficiently, adding proper "team" references to the user attributes.

---
+++ connector/forgejo/src/main/resources/User.team.support.search.groovy
@@ -0,0 +1,33 @@
+import org.identityconnectors.framework.common.objects.ConnectorObjectReference
+
+objectClass("User") {
+    search {
+        attributeResolver {
+            attribute "team"
+            resolutionType PER_OBJECT
+            implementation {
+                var userTeams = new ArrayList()
+                var orgFilter = objectClass("Organization").attributeFilter("member").eq(value)
+                // Let's get all user organizations
+                var orgs = objectClass("Organization").search(orgFilter)
+
+                // For each organization we need to find teams
+                for (var org : orgs) {
+                    def teamFilter = attributeFilter("organization.name").eq(org.name.nameValue)
+                    teamsInOrg = objectClass("Team").search(teamFilter, resultHandler)
+                    // For each team we need to get it's all members, because there is no endpoint which allows us
+                    // to search teams by it's member.
+                    for (var team : teamsInOrg) {
+                        var teamMembersFilter = objectClass("User").attributeFilter("team")
+                            .eq(team);
+                        usersInTeam = objectClass("User").search(teamMembersFilter)
+                        if (usersInTeam.any {it.uid.equals(value.build().uid)}) {
+                            userTeams.add(new ConnectorObjectReference(team))
+                        }
+                    }
+                }
+                value.addAttribute("team", userTeams)
+            }
+        }
+    }
+}
