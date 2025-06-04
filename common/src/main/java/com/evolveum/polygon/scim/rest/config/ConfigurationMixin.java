f6b98637094af0a7b1e200e74e90056f2cc334d1
Tony Tkáčik <tonydamage@gmail.com>
2025-06-04 16:02:49 +0200

    Moved `require` method to ConfigurationMixin for type casting
    This method ensures safe casting to extended configuration types and throws an exception if the current instance is incompatible. It improves type safety and simplifies handling of extended configurations.

--- common/src/main/java/com/evolveum/polygon/scim/rest/config/ConfigurationMixin.java
+++ common/src/main/java/com/evolveum/polygon/scim/rest/config/ConfigurationMixin.java
@@ -7,4 +7,23 @@ package com.evolveum.polygon.scim.rest.config;
  * of the connector.
  */
 public interface ConfigurationMixin {
+
+
+    /**
+     * Returns extended configuration such as {@link RestClientConfiguration.BasicAuthorization} or {@link RestClientConfiguration.TokenAuthorization}.
+     *
+     * Default implementation returns the current instance casted to extended configuration
+     * otherwise throws an {@link IllegalStateException}.
+     *
+     * @param <E> the type of the extended configuration
+     * @param extendedConfiguration the class of the extended configuration type to cast to
+     * @return an instance of the specified extended configuration type
+     * @throws IllegalStateException if the current instance is not of the specified type
+     */
+    default  <E extends ConfigurationMixin> E require(Class<E> extendedConfiguration) {
+        if (extendedConfiguration.isInstance(this)) {
+            return extendedConfiguration.cast(this);
+        }
+        throw new IllegalStateException("Configuration of type " + extendedConfiguration.getSimpleName() + " required");
+    }
 }
