78dd86ba94a900a8eec301ee55c3eb83a05f9194
mmacik <matus.macik@evolveum.com>
2025-09-23 12:01:46 +0200

    Open project connector, native reference group - user, object subject

--- connector/open-project/src/main/resources/Group.native.schema.groovy
+++ connector/open-project/src/main/resources/Group.native.schema.groovy
@@ -1,3 +1,7 @@
+import org.identityconnectors.framework.common.objects.ConnectorObjectBuilder
+import org.identityconnectors.framework.common.objects.ConnectorObjectReference
+import org.identityconnectors.framework.common.objects.ObjectClass
+
 /*
  * Copyright (c) 2025 Evolveum and contributors
  *
@@ -44,5 +48,26 @@ objectClass("Group") {
         returnedByDefault true;
         required false;
         description "Time of the most recent change to the user";
+    }
+    reference("members") {
+        description("The list all all the users that are members of the group")
+        objectClass "User"
+
+        json {
+            path attribute("_links").child("members")
+            implementation {
+                deserialize {
+
+                    var href = value.get("href")?.asText();
+                    var pid = href.substring(href.lastIndexOf("/") + 1)
+
+                    var obj = new ConnectorObjectBuilder()
+                            .setObjectClass(new ObjectClass("User"))
+                            .setUid(pid)
+                            .setName(value.get("title")?.asText())
+                    return new ConnectorObjectReference(obj.build());
+                }
+            }
+        }
     }
 }
