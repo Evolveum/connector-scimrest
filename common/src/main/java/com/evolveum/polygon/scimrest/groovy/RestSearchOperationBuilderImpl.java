93de7111e234e223dc8d45db0a7546677cff36f2
Jan Kamenicky <jan.kamenicky@evolveum.com>
2026-08-20 11:13:19 +0000

    Deduplicate normalization search wiring
    Now that conndev exposes buildNormalizationHandler() and
    buildFilterDispatcher() as protected, RestSearchOperationBuilderImpl
    only needs to override isEmpty() and buildFilterDispatcher() (for
    SCIM's dual-slot dispatch) instead of duplicating normalize(),
    build(), and buildAttributeResolver() wholesale.
    
    Task: 11964

--- common/src/main/java/com/evolveum/polygon/scimrest/groovy/RestSearchOperationBuilderImpl.java
+++ common/src/main/java/com/evolveum/polygon/scimrest/groovy/RestSearchOperationBuilderImpl.java
@@ -10,14 +10,10 @@ import com.evolveum.polygon.conndev.concepts.DefinitionValue;
 import com.evolveum.polygon.conndev.concepts.GroovyClosures;
 import com.evolveum.polygon.conndev.groovy.AbstractSearchOperationBuilder;
 
-import com.evolveum.polygon.conndev.build.api.NormalizationBuilder;
 import com.evolveum.polygon.conndev.build.api.SearchOperationBuilder;
 import com.evolveum.polygon.conndev.build.api.SearchScriptBuilder;
-import com.evolveum.polygon.conndev.spi.AttributeResolver;
 import com.evolveum.polygon.scimrest.groovy.api.RestSearchOperationBuilder;
-import com.evolveum.polygon.conndev.spi.AttributeResolvingSearchHandler;
 import com.evolveum.polygon.conndev.spi.FilterBasedSearchDispatcher;
-import com.evolveum.polygon.conndev.schema.BaseAttributeDefinition;
 import com.evolveum.polygon.scimrest.impl.scim.ScimSearchHandler;
 import com.evolveum.polygon.conndev.spi.ObjectSearchOperation;
 import com.evolveum.polygon.conndev.spi.FilterAwareExecuteQueryProcessor;
@@ -27,13 +23,11 @@ import groovy.lang.DelegatesTo;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 public class RestSearchOperationBuilderImpl extends AbstractSearchOperationBuilder implements RestSearchOperationBuilder, RestObjectOperationBuilder<ObjectSearchOperation> {
 
     private final BaseOperationSupportBuilder restParent;
     Map<String, EndpointBasedSearchBuilder<?,?>> endpointBuilder = new HashMap<>();
-    private NormalizationBuilderImpl normalizationBuilder;
     private ScimSearchHandler.Builder scim;
     private DefinitionValue<Boolean> enabled = DefinitionValue.DEFAULT_TRUE;
 
@@ -78,32 +72,12 @@ public class RestSearchOperationBuilderImpl extends AbstractSearchOperationBuild
     }
 
     @Override
-    public NormalizationBuilder normalize() {
-        if (normalizationBuilder == null) {
-            normalizationBuilder = new NormalizationBuilderImpl();
-        }
-        return normalizationBuilder;
+    protected boolean isEmpty() {
+        return builders.isEmpty() && scim == null;
     }
 
     @Override
-    public ObjectSearchOperation build() {
-        if (builders.isEmpty() && scim == null) {
-            // We don't have any endpoints, so we don't need to build anything, this results in search operation
-            // being unsupported.
-            return null;
-        }
-
-        return buildAttributeResolver(buildNormalizationHandler(buildFilterDispatcher()));
-    }
-
-    private ObjectSearchOperation buildNormalizationHandler(ObjectSearchOperation executeQueryProcessor) {
-        if (normalizationBuilder == null) {
-            return executeQueryProcessor;
-        }
-        return normalizationBuilder.build(executeQueryProcessor);
-    }
-
-    private ObjectSearchOperation buildFilterDispatcher() {
+    protected ObjectSearchOperation buildFilterDispatcher() {
         var handlers = new HashSet<FilterAwareExecuteQueryProcessor>();
         ObjectSearchOperation emptyFilterHandler = null;
         ObjectSearchOperation anyFilterHandler = null;
@@ -132,40 +106,6 @@ public class RestSearchOperationBuilderImpl extends AbstractSearchOperationBuild
             }
         }
         return new FilterBasedSearchDispatcher<>(emptyFilterHandler, anyFilterHandler,  handlers);
-    }
-
-    private ObjectSearchOperation buildAttributeResolver(ObjectSearchOperation dispatcher) {
-        Set<AttributeResolver> perObjectResolvers = new HashSet<>();
-        Set<AttributeResolver> batchedResolvers = new HashSet<>();
-        Set<BaseAttributeDefinition> supportedAttributes = new HashSet<>();
-        for (var builder : resolvers) {
-            var resolver = builder.build();
-            supportedAttributes.addAll(resolver.getSupportedAttributes());
-            switch (builder.resolutionType()) {
-                case BATCH -> batchedResolvers.add(resolver);
-                case PER_OBJECT -> perObjectResolvers.add(resolver);
-                default -> throw new IllegalStateException("Unknown resolver type: " + builder.resolutionType());
-            }
-        }
-        for (var attribute : restParent.getObjectClass().attributes()) {
-            if (attribute.emulated()) {
-                var resolver = attribute.resolver();
-                if (resolver == null && !supportedAttributes.contains(attribute)) {
-                    throw new IllegalStateException("Attribute: " + attribute.remoteName() + " is emulated, but no resolver exists.");
-                }
-                switch (resolver.resolutionType()) {
-                    case BATCH -> batchedResolvers.add(resolver);
-                    case PER_OBJECT -> perObjectResolvers.add(resolver);
-                    default -> throw new IllegalStateException("Unknown resolver type: " + resolver.resolutionType());
-                }
-            }
-        }
-
-        if (!perObjectResolvers.isEmpty() || !batchedResolvers.isEmpty()) {
-            dispatcher = new AttributeResolvingSearchHandler(dispatcher, perObjectResolvers, batchedResolvers);
-        }
-
-        return dispatcher;
     }
 
     @Override
